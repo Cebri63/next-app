5c353c150562f92a439b7d9c562528d403d2f894
Brice LESNE <brice@pc9.home>
2019-12-23 15:21:27 +0100

    first Next.js App

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/MyLayout";
+
+const About = () => {
+  return (
+    <Layout>
+      <div>About Page</div>
+    </Layout>
+  );
+};
+
+export default About;
